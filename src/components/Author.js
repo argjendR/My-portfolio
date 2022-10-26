53212960b0d82e9f5da09b2096375e54402477e1
argjendR <gentxh@gmail.com>
2022-10-26 23:16:15 +0200

    second commit

--- src/components/Author.js
+++ src/components/Author.js
@@ -1,7 +1,8 @@
 const Author = (props) => {
     return (
         <div>
-            Author of the portfolio Argjend github change
+            <h1>Author of the portfolio Argjend github change</h1>
+            <p>shoud be changed</p>
         </div>
     )
      
