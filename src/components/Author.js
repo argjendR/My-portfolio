6f08d2b8c173e55bf4df51315ce53a63cfff4664
argjendR <gentxh@gmail.com>
2022-11-01 15:04:14 +0100

    third commit

--- src/components/Author.js
+++ src/components/Author.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Author(props) {
   return (
     <div>
-      {props.name}
+      <h1>{props.name}</h1>
+      <h2>{props.email} </h2>
+      <h2>{props.phone} </h2>
+      <h2>{props.webpage}</h2>
+      <img src="src\logo.svg" alt="portret" />
+      
+
     </div>
   )
 }
