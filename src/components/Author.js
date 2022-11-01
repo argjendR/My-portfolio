81701da8104df2c5252da08640d67a4f33e48ef8
argjendR <gentxh@gmail.com>
2022-11-01 15:10:04 +0100

    third commit

--- src/components/Author.js
+++ src/components/Author.js
@@ -6,8 +6,10 @@ function Author(props) {
       <h1>{props.name}</h1>
       <h2>{props.email} </h2>
       <h2>{props.phone} </h2>
-      <h2>{props.webpage}</h2>
-      <img src="src\logo.svg" alt="portret" />
+      <script src="www.google.com"></script>
+      <img src="src\logo.svg" alt="portret"
+      
+      />
       
 
     </div>
